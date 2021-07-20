43ed7088387a91262b12beaea4b0e62fb4743097
mdes4214 <mdes4214@gmail.com>
2021-07-20 22:13:40 +0800

    move test folder and add in-memory mongodb

--- backend/src/test/server.spec.ts
+++ backend/src/test/server.spec.ts
@@ -1,22 +1,23 @@
 import { FastifyInstance } from 'fastify'
-import { startFastify } from '../src/server'
+import { startFastify } from '../server'
 import { Server, IncomingMessage, ServerResponse } from 'http'
+import * as dbHandler from './db'
 
 describe('API test', () => {
     let server: FastifyInstance<Server, IncomingMessage, ServerResponse>
 
     beforeAll(async () => {
-        // await dbHandler.connect()
+        await dbHandler.connect()
         server = startFastify(8888)
     })
     
-    // afterEach(async () => {
-    //     await dbHandler.clearDatabase()
-    // })
+    afterEach(async () => {
+        await dbHandler.clearDatabase()
+    })
     
     afterAll(async () => {
         try {
-            // await dbHandler.closeDatabase()
+            await dbHandler.closeDatabase()
             server.close((): void => { })
             console.log('Closing Fastify server is done!')
         } catch (e) {
@@ -29,5 +30,12 @@ describe('API test', () => {
 
         expect(response.statusCode).toBe(200)
         expect(response.body).toStrictEqual(JSON.stringify({ msg: 'pong' }))
+    })
+
+    it('should successfully get a empty list of cats', async () => {
+        const response = await server.inject({ method: 'GET', url: '/cats' })
+
+        expect(response.statusCode).toBe(200)
+        expect(response.body).toStrictEqual(JSON.stringify({ cats: [] }))
     })
 })
